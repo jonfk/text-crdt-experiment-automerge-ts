7c684d8d4cd6592aa5fd980b4a04c44089c9f479
Jonathan Fok Kan <jfokkan@gmail.com>
2020-01-18 14:57:00 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/SaveButton';
+import SaveButton from './components/SaveButton';
+import {Helmet} from "react-helmet";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                  <Helmet>
+                <meta charSet="utf-8" />
+                <title>My Title</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
+      <textarea rows={10} cols={40}></textarea>
+      <SaveButton />
     </div>
   );
 }
---
+++ src/components/SaveButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SaveButton: React.FC = () => {
+    return (
+        <button>Save</button>
+    );
+}
+
+export default SaveButton;
